87cfaf63fdf96c9ea54f86c1c321f35fbd36ce59
Sasha Myasoedov <smyasoedov@cybervisiontech.com>
2014-09-05 16:23:15 +0300

    Package setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+exec(open('./version.py').read())
+
+setup(
+    name='bandwith_sdk',
+    version=__version__,
+    description='This client library is designed to support the Bandwith '
+                'API and the official Bandwith SDK, which '
+                'is the canonical way to implement Facebook authentication.',
+    author='Bandwith',
+    maintainer='Bandwith',
+    maintainer_email='classifieds-admin@bandwidth.com',
+    url='',
+    license='Apache',
+    packages=["bandwith_sdk"],
+    long_description=open("README.md").read(),
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+    ],
+    install_requires=[
+        'requests',
+    ],
+)
