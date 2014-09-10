042f294a4e97db8a97b3bebff40a0e9e3fc719d0
Sasha Myasoedov <smyasoedov@cybervisiontech.com>
2014-09-10 14:57:26 +0300

    Clean example from comments

--- web_v4.py
+++ web_v4.py
@@ -19,21 +19,10 @@ NoSqlStorage = {}
 @app.route('/v1/events', methods=['POST'])
 def events_v1():
     event = json.loads(request.data.decode('ascii'))
-    # Example of deserialized event:
-    # {
-    #     'time': '2014-07-23T13:04:39Z',
-    #     'applicationId': 'a-73mq4rpnghrtbqprtr45zdy',
-    #     'eventType': 'incomingcall',
-    #     'callState': 'active',
-    #     'callId': 'c-pkyusdpacfmvmopvd7p3dmi',
-    #     'callUri': 'https://127.0.0.1/v1/users/u-2qep46jwram5oyyqqa5muli/calls/c-pkyusdpacfmvmopvd7p3dmi',
-    #     'from': '+14084782637',
-    #     'to': '+14082149398'
-    # }
     event = Event.create(**event)
     #event = Event.create(request.data)
 
-    call = event.call  # or Call('c-pkyusdpacfmvmopvd7p3dmi')
+    call = event.call  # Call('c-pkyusdpacfmvmopvd7p3dmi')
     if isinstance(event, AnswerCallEvent):
         do_play_audio.delay(call, 'Hello.mp3')
     elif isinstance(event, PlaybackCallEvent):
@@ -57,7 +46,7 @@ def do_play_audio(call_id, file):
 
 @job('default')
 def do_bridge(*calls):
-    Bridge.create(*calls)
+    bridge = Bridge.create(*calls)
 
 
 app.debug = True
