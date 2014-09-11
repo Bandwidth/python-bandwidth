929993fbcfdc4e87b83e1b3aa2cd33a4b2ead0ac
Sasha Myasoedov <smyasoedov@cybervisiontech.com>
2014-09-11 16:40:12 +0300

    Ported call object from v4 client

--- bandwith_sdk/models.py
+++ bandwith_sdk/models.py
@@ -1,3 +1,9 @@
+# Object models for SDK
+from .client import Client
+
+# Sentinel value to mark that some of properties have been not synced.
+UNEVALUATED = object()
+
 
 class Resource(object):
     client = None
@@ -30,3 +36,149 @@ class Resource(object):
         :return:
         """
         raise NotImplemented
+
+
+class Call(object):
+    call_id = None
+    direction = None
+    from_ = None
+    to = None
+    recording_enabled = None
+    callback_url = None
+    state = None
+    start_time = None
+    active_time = None
+    client = None
+
+    def __init__(self, data: dict):
+        self.client = Client()
+        if isinstance(data, dict):
+            self.data = data
+            self.set_up()
+        elif isinstance(data, str):
+            self.data = UNEVALUATED
+            self.call_id = data
+        else:
+            raise TypeError('Accepted only call-id or call data as dictionary')
+
+    def set_up(self):
+        self.call_id = self.data.get('id') or self.data.get('callId')
+        self.direction = self.data.get('direction')
+        self.from_ = self.data.get('from')
+        self.to = self.data.get('to')
+        self.recording_enabled = self.data.get('recordingEnabled')
+        self.callback_url = self.data.get('callbackUrl')
+        self.state = self.data.get('state')
+        self.start_time = self.data.get('startTime')
+        self.active_time = self.data.get('activeTime')
+
+    @classmethod
+    def create(cls, caller, callee, **kwargs):
+        client = cls.client or Client()
+        url = 'calls/'
+
+        json_data = {
+            'from': caller,
+            'to': callee,
+            'callTimeout': 30,  # seconds
+        }
+
+        json_data.update(kwargs)
+        data = client._post(url, data=json_data)
+        location = data.headers['Location']
+        call_id = location.split('/')[-1]
+        return cls(call_id)
+
+    @classmethod
+    def get(cls, *args, **kwargs):
+        client = cls.client or Client()
+        data_as_dict = client.get_call_info(*args, **kwargs)
+        return cls(data_as_dict)
+
+    @classmethod
+    def list(cls, *args, **kwargs):
+        client = cls.client or Client()
+        data_as_list = client.get_calls(*args, **kwargs)
+        return [cls(v) for v in data_as_list]
+
+    def play_audio(self, timeout=None, **kwargs):
+        '''
+        Plays audio form the given url to the call associated with call_id
+        '''
+        url = 'calls/{}/audio'.format(self.call_id)
+
+        # stops currently playing audio
+        key = 'fileUrl' if 'fileUrl' in kwargs else 'sentence'
+        if kwargs[key]:
+            self.client._post(url, data={key: ''}, timeout=timeout)
+
+        return self.client._post(url, data=kwargs, timeout=timeout)
+
+    def set_call_property(self, timeout=None, **kwargs):
+        url = 'calls/{}'.format(self.call_id)
+        return self.client._post(url, data=kwargs, timeout=timeout)
+
+    def bridge(self, *calls, bridge_audio=True):
+        _calls = (self,) + calls
+        return Bridge.create(*_calls, bridge_audio=bridge_audio)
+
+    def refresh(self):
+        url = 'calls/{}'.format(self.call_id)
+        data = self.client._get(url).json()
+        self.data.update(data)
+        self.set_up()
+
+    def __repr__(self):
+        return 'Call({})'.format(repr(self.data) if self.data is not UNEVALUATED else self.call_id)
+
+    def send_dtmf(self, dtmf, timeout=None):
+        '''
+        Sends a string of characters as DTMF on the given call_id
+        Valid chars are '0123456789*#ABCD'
+        '''
+        url = 'calls/{}/dtmf'.format(self.call_id)
+
+        json_data = {'dtmfOut': dtmf}
+
+        return self.client._post(url, data=json_data, timeout=timeout)
+
+    def receive_dtmf(self, max_digits, terminating_digits,
+                     inter_digit_timeout='1', timeout=None):
+        url = 'calls/{}/gather'.format(self.call_id)
+
+        http_get_params = {
+            'maxDigits': max_digits,
+            'terminatingDigits': terminating_digits,
+            'interDigitTimeout': inter_digit_timeout}
+
+        return self.client._get(url, params=http_get_params, timeout=timeout)
+
+    def hangup(self):
+        '''
+        Hangs up a call with the given call_id
+        '''
+        url = 'calls/{}/'.format(self.call_id)
+
+        return self.client._post(url, data={}, timeout=None)
+
+    def activate_gathering(self, maxDigits, interDigitTimeout, terminatingDigits, timeout=None, **kwargs):
+        url = 'calls/{}/gather'.format(self.call_id)
+        data = {'maxDigits': maxDigits,
+                'interDigitTimeout': interDigitTimeout,
+                'terminatingDigits': terminatingDigits,
+                'prompt': kwargs.get('prompt')}
+        data = {k: v for k, v in data.items() if v is not None}  # deleting keys with None values
+        return self.client._post(url, data=data, timeout=timeout)
+
+    def get_gather_info(self, gather_id, timeout=None):
+        url = 'calls/{}/gather/{}'.format(self.call_id, gather_id)
+        return self.client._get(url, timeout=timeout)
+
+    def get_recordings(self, timeout=None):
+        '''
+        Retrieves an array with all the recordings of the call_id
+        '''
+        url = 'calls/{}/recordings'.format(self.call_id)
+
+        return self.client._get(url, timeout=timeout).json()
+
