d65960d3ad49b24ebc52221784dcacb77aed2b2f
Sasha Myasoedov <smyasoedov@cybervisiontech.com>
2014-09-12 14:09:50 +0300

    Fixed call create

--- tests/test_models.py
+++ tests/test_models.py
@@ -34,3 +34,62 @@ class CallsTest(unittest.TestCase):
         call = Call.get('c-call-id')
 
         self.assertEqual(call.call_id, 'c-call-id')
+
+    @responses.activate
+    def test_list(self):
+        """
+        Call.list()
+        """
+        raw = """
+        [{
+        "id": "c-call-id",
+        "direction": "out",
+        "from": "+1919000001",
+        "to": "+1919000002",
+        "recordingEnabled": false,
+        "callbackUrl": "",
+        "state": "active",
+        "startTime": "2013-02-08T13:15:47.587Z",
+        "activeTime": "2013-02-08T13:15:52.347Z",
+        "events": "https://.../calls/{callId}/events"
+        },
+        {
+        "id": "c-call-id-1",
+        "direction": "out",
+        "from": "+1919000001",
+        "to": "+1919000002",
+        "recordingEnabled": false,
+        "callbackUrl": "",
+        "state": "active",
+        "startTime": "2013-02-08T13:15:47.587Z",
+        "activeTime": "2013-02-08T13:15:52.347Z",
+        "events": "https://.../calls/{callId}/events"
+        }
+        ]
+        """
+
+        responses.add(responses.GET,
+                      'https://api.catapult.inetwork.com/v1/users/u-user/calls/',
+                      body=raw,
+                      status=200,
+                      content_type='application/json')
+        calls = Call.list()
+
+        self.assertEqual(calls[0].call_id, 'c-call-id')
+        self.assertEqual(calls[1].call_id, 'c-call-id-1')
+
+    @responses.activate
+    def test_create(self):
+        """
+        Call.create("+1919000001", "+1919000002")
+        """
+        responses.add(responses.POST,
+                      'https://api.catapult.inetwork.com/v1/users/u-user/calls/',
+                      body='',
+                      status=201,
+                      content_type='application/json',
+                      adding_headers={'Location': '/v1/users/u-user/calls/new-call-id'})
+
+        call = Call.create("+1919000001", "+1919000002")
+
+        self.assertEqual(call.call_id, 'new-call-id')
