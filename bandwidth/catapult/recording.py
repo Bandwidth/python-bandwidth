577bcb2241f2e8874d842851299f251a4d4a109c
avbel <belchikovs@gmail.com>
2016-11-08 14:41:44 +0300

    Fixed tests on python 3

--- bandwidth/catapult/recording.py
+++ bandwidth/catapult/recording.py
@@ -1,5 +1,8 @@
-from itertools import imap
+import six
 from .lazy_enumerable import get_lazy_enumerator
+if six.PY2:
+    from itertools import imap
+    map = imap  # to use lazy map on python 2
 
 
 def _set_media_name(recording):
@@ -10,7 +13,7 @@ def _set_media_name(recording):
 class RecordingMixin:
     def get_recordings(self, query=None):
         path = '/users/%s/recordings' % self.user_id
-        return imap(_set_media_name, get_lazy_enumerator(self, lambda: self._make_request('get', path, params=query)))
+        return map(_set_media_name, get_lazy_enumerator(self, lambda: self._make_request('get', path, params=query)))
 
     def get_recording(self, id):
         path = '/users/%s/recordings/%s' % (self.user_id, id)
