6ad014e1178ae38860102d97fb065d0d0605b4ac
avbel <belchikovs@gmail.com>
2016-11-14 10:03:47 +0300

    Added docs for Media

--- bandwidth/catapult/media.py
+++ bandwidth/catapult/media.py
@@ -6,11 +6,47 @@ quote = urllib.parse.quote if six.PY3 else urllib.quote
 
 
 class MediaMixin:
+    """
+    Media API
+    """
     def get_media_files(self):
+        """
+        Gets a list of user's media files.
+
+        :rtype: types.GeneratorType
+        :returns: list of media files
+
+        :Example:
+        list = api.get_media_files()
+
+        """
         path = '/users/%s/media' % self.user_id
         return get_lazy_enumerator(self, lambda: self._make_request('get', path))
 
     def upload_media_file(self, media_name, content=None, content_type='application/octet-stream', file_path=None):
+        """
+        Upload a file
+
+        :type media_name: str
+        :param media_name: name of file on bandwidth server
+
+        :type content: str|buffer|bytearray|stream|file
+        :param content: content of file to upload (file object, string or buffer).
+        Don't use together with file_path
+
+        :type content_type: str
+        :param content_type: mime type of file
+
+        :type file_path: str
+        :param file_path: path to file to upload. Don't use together with content
+
+        :Example:
+        api.upload_media_file('file1.txt', 'content of file', 'text/plain')
+
+        # with file path
+        api.upload_media_file('file1.txt', file_path='/path/to/file1.txt')
+
+        """
         is_file_path = False
         if file_path is not None and content is None:
             content = open(file_path, 'rb')
@@ -23,11 +59,32 @@ class MediaMixin:
                 content.close()
 
     def download_media_file(self, media_name):
+        """
+        Download a file
+
+        :type media_name: str
+        :param media_name: name of file on bandwidth server
+
+        :rtype (stream, str)
+        :returns stream to file to download and mime type
+
+        :Example:
+        stream, content_type = api.download_media_file('file1.txt')
+        """
         path = '/users/%s/media/%s' % (self.user_id, quote(media_name))
         response = self._request('get', path, stream=True)
         response.raise_for_status()
         return response.raw, response.headers['content-type']
 
     def delete_media_file(self, media_name):
+        """
+        Remove a file from the server
+
+        :type media_name: str
+        :param media_name: name of file on bandwidth server
+
+        :Example:
+        api.delete_media_file('file1.txt')
+        """
         path = '/users/%s/media/%s' % (self.user_id, quote(media_name))
         self._make_request('delete', path)
