b29f9279a1cd4697094a46d788a49c7eb48fe8b7
Dan Tolbert <dtolbert@bandwidth.com>
2017-02-11 14:25:19 -0500

    update _ to from

--- bandwidth/catapult/__init__.py
+++ bandwidth/catapult/__init__.py
@@ -2790,7 +2790,7 @@ class Client:
         Example: Send Picture Message::
 
             id = api.send_message(
-                from = '+1234567980',
+                from_ = '+1234567980',
                 to = '+1234567981',
                 media = ['http://host/path/to/file']
                 )
