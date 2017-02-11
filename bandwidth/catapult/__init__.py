c49f635a40a6bb82409afd74a13bd6ce7d56cda6
Dan Tolbert <dtolbert@bandwidth.com>
2017-02-11 14:16:03 -0500

    update sendmessage to sendmessages

--- bandwidth/catapult/__init__.py
+++ bandwidth/catapult/__init__.py
@@ -2845,7 +2845,7 @@ class Client:
 
         Example: Bulk Send Picture or Text messages (or both)::
 
-            results = api.send_message([
+            results = api.send_messages([
                 {'from': '+1234567980', 'to': '+1234567981', 'text': 'SMS message'},
                 {'from': '+1234567980', 'to': '+1234567982', 'text': 'SMS message2'}
             ])
