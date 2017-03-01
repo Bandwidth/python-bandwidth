0490907c5f4c2766a47b860596a58c3e02ffecb6
Dan Tolbert <danielptolbert@gmail.com>
2017-03-01 18:28:18 -0500

    update stuff

---
+++ replace.py
@@ -0,0 +1,23 @@
+import re
+def convert_comment_string_to_snake_case(s):
+  """
+  Changes String to from camelCase to snake_case
+  :param s: String to convert
+  :rtype: String
+  :rertuns: String converted to snake_case
+  """
+  regex = r"(\s+##\s+\'\w+)((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))(\w+\')"
+  a = re.compile("(##\s+\'\w+)((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))(\w+\')")
+  # m.group(1)sub(r'_\1', s).lower()
+
+  result = re.match(regex, s)
+  if result:
+    print(result.group(0) + result.group(1).lower() + result.group(2))
+    return a.sub(r'\1_\2\3', s).lower()
+  else:
+    return s
+
+with open('bandwidth/catapult/__init__.py') as f:
+    with open('out2.py', 'w') as e:
+      for line in f:
+        e.write(convert_comment_string_to_snake_case(line))
