9d6ce2a4fb7710b74c9c315fb6d058180d282696
Aubron Wood <aubron@daemon.group>
2019-03-25 12:02:04 -0400

    Fix indentation
    I hate you too, python

--- bandwidth/messaging/client_module.py
+++ bandwidth/messaging/client_module.py
@@ -68,7 +68,7 @@ class Client:
     def _check_response(self, response):
         if response.status_code >= 400:
             if response.headers.get('content-type') is not None and \
-                response.headers.get('content-type').startsWith("application/json"):
+                    response.headers.get('content-type').startsWith("application/json"):
                 data = response.json()
                 raise BandwidthMessageAPIException(
                     response.status_code, data['message'], code=data.get('code'))
@@ -82,7 +82,7 @@ class Client:
         data = None
         id = None
         if response.headers.get('content-type') is not None and \
-            response.headers.get('content-type').startsWith("application/json"):
+                response.headers.get('content-type').startsWith("application/json"):
             data = convert_object_to_snake_case(response.json())
         location = response.headers.get('location')
         if location is not None:
--- bandwidth/voice/client_module.py
+++ bandwidth/voice/client_module.py
@@ -77,7 +77,7 @@ class Client:
     def _check_response(self, response):
         if response.status_code >= 400:
             if response.headers.get('content-type') is not None and \
-                response.headers.get('content-type').startsWith("application/json"):
+                    response.headers.get('content-type').startsWith("application/json"):
                 data = response.json()
                 raise BandwidthVoiceAPIException(
                     response.status_code, data['message'], code=data.get('code'))
@@ -91,7 +91,7 @@ class Client:
         data = None
         id = None
         if response.headers.get('content-type') is not None and \
-            response.headers.get('content-type').startsWith("application/json"):
+                response.headers.get('content-type').startsWith("application/json"):
             data = convert_object_to_snake_case(response.json())
         location = response.headers.get('location')
         if location is not None:
